7c435096f2f4aa3bf57284b088681e895bcd5439
unknown <>
2016-05-29 03:02:25 +0900

    user

--- android/app/src/main/java/com/sosoham/sosoham/HelloActivity.java
+++ android/app/src/main/java/com/sosoham/sosoham/HelloActivity.java
@@ -1,7 +1,13 @@
 package com.sosoham.sosoham;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.andtinder.model.CardModel;
+import com.andtinder.model.Orientations;
+import com.andtinder.view.CardContainer;
+import com.andtinder.view.SimpleCardStackAdapter;
 
 public class HelloActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+
+        CardContainer mCardContainer = (CardContainer) findViewById(R.id.layoutview);
+        mCardContainer.setOrientation(Orientations.Orientation.Ordered);
+
+        CardModel card = new CardModel("Title1", "Description goes here", this.getDrawable(R.drawable.sohy04));
+
+
+
+        card.setOnClickListener(new CardModel.OnClickListener() {
+            @Override
+            public void OnClickListener() {
+                Log.i("Swipeable Cards", "I am pressing the card");
+            }
+        });
+        
+        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(this);
+        adapter.add(new CardModel("Title1", "Description goes here", this.getDrawable(R.drawable.sohy04)));
+        mCardContainer.setAdapter(adapter);
+
+
+
+
+
+
+
     }
 }
