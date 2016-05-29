e30830e4b750d7e5baa9634e71378cbef9e759a3
kimsangyoun <ksang91@naver.com>
2016-05-29 09:08:06 +0900

    123

--- android/app/src/main/java/com/sosoham/sosoham/MainActivity.java
+++ android/app/src/main/java/com/sosoham/sosoham/MainActivity.java
@@ -1,15 +1,12 @@
 package com.sosoham.sosoham;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.sosoham.sosoham.Activity.HelloActivity;
 import com.sosoham.sosoham.sosodb.FaceBook;
-import com.sosoham.sosoham.sosodb.SOSODB;
-
-import org.json.JSONObject;
 
 /**
  * Created by mac on 2016. 5. 29..
